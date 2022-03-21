133e0fbfbfa333698f5a4fcf9e3a4a48f90c960c
Łukasz Kosturski <lukasz.kosturski@gmail.com>
2022-03-21 14:38:46 +0100

    first addition

--- 04-git/Program.java
+++ 04-git/Program.java
@@ -7,11 +7,13 @@ class Main {
     int first = 10;
     int second = 20;
     int third = 30;
+    System.out.println("First addition");
     
     System.out.println(first  + " and " + second);
 
     // add two numbers
     int sum = first + second;
+    System.out.println("Second addition");
     System.out.println("The sum is: " + sum);
     System.out.println("Third addition");
   }
